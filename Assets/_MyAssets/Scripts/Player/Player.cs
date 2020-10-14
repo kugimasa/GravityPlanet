1cf494ff74d37c3f53cfa8d951329e6ca9e303b9
kugimasa <kugimania@gmail.com>
2020-10-14 21:57:18 +0900

    add: Planet1=>Planet2=>Planet3への移動

--- Assets/_MyAssets/Scripts/Player/Player.cs
+++ Assets/_MyAssets/Scripts/Player/Player.cs
@@ -1,30 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Rails;
+using UnityEngine.Serialization;
 
 namespace Player
 {
     [RequireComponent(typeof(Rigidbody))]
     public class Player : MonoBehaviour
     {
-        [SerializeField] private float gravityScale = 9.8f;
-        [SerializeField] private float walkSpeed = 3f;
-        [SerializeField] private LayerMask groundLayer = default;
-        [SerializeField] private float distanceOnGround = 1.5f;
-
-        private Transform cashedTransform = default;
-        private Rigidbody cashedRigidbody = default;
-
-        private float speedAlongGravity = 0f;
+        [SerializeField] private float m_gravityScale = 9.8f;
+        [SerializeField] private float m_walkSpeed = 3f;
+        [SerializeField] private LayerMask m_groundLayer = default;
+        [SerializeField] private float m_distanceOnGround = 1.5f;
+        [SerializeField] private float m_pathDuration = 1.0f;
+        [SerializeField] private Transform m_currentPlanet = default;
+        
+        private Transform m_Transform = default;
+        private Rigidbody m_Rigidbody = default;
+        private Rail m_Rail = default;
+        
+        private float m_speedAlongGravity = 0f;
 
         private CinemachineSmoothPath m_nowCinemachine;
-        private float m_pathStartTime=0f;
-        [SerializeField] float m_pathDuration = 1.0f;
+        private float m_pathStartTime = 0f;
         private void Start()
         {
-            this.cashedTransform = this.transform;
-            this.cashedRigidbody = this.GetComponent<Rigidbody>();
+            m_Transform = transform;
+            m_Rigidbody = GetComponent<Rigidbody>();
         }
 
         private void OnTriggerEnter(Collider other)
@@ -32,6 +37,8 @@ namespace Player
             if (other.CompareTag("Rail"))
             {
                 var path = other.GetComponent<CinemachineSmoothPath>();
+                m_Rail = other.GetComponent<Rail>();
+                m_currentPlanet = m_Rail.GoalPlanet;
                 m_nowCinemachine = path;
                 m_pathStartTime = Time.time;
             }
@@ -43,44 +50,48 @@ namespace Player
             {
                 //シネマシン移動処理
                 var t = Time.time - m_pathStartTime;
-                cashedTransform.position=m_nowCinemachine.EvaluatePosition(t/m_pathDuration);
+                m_Transform.position = m_nowCinemachine.EvaluatePositionAtUnit(t/m_pathDuration, CinemachinePathBase.PositionUnits.Normalized);
+                if (t > m_pathDuration)
+                {
+                    m_nowCinemachine = null;
+                }
             }
         }
 
         private void FixedUpdate()
         {
             // 重力.
-            var gravityDirection = (Vector3.zero - this.cashedTransform.position).normalized; // 原点が重力源.
+            var gravityDirection = (m_currentPlanet.position - this.m_Transform.position).normalized; // 原点が重力源.
             // 地面に立っている時は重力方向の速さを0にする.
-            if (Physics.Raycast(this.cashedTransform.position, gravityDirection, this.distanceOnGround, this.groundLayer.value))
+            if (Physics.Raycast(this.m_Transform.position, gravityDirection, this.m_distanceOnGround, this.m_groundLayer.value))
             {
-                this.speedAlongGravity = 0f;
+                this.m_speedAlongGravity = 0f;
             }
             else
             {
-                this.speedAlongGravity += this.gravityScale * Time.fixedDeltaTime;
+                this.m_speedAlongGravity += this.m_gravityScale * Time.fixedDeltaTime;
             }
-            var gravityVelocity = gravityDirection * this.speedAlongGravity;
+            var gravityVelocity = gravityDirection * this.m_speedAlongGravity;
 
             // 回転.
             var upwards = -gravityDirection;
-            var forward = Vector3.ProjectOnPlane(this.cashedTransform.forward, upwards);
-            this.cashedTransform.rotation = Quaternion.LookRotation(forward, upwards);
+            var forward = Vector3.ProjectOnPlane(this.m_Transform.forward, upwards);
+            this.m_Transform.rotation = Quaternion.LookRotation(forward, upwards);
 
             // 移動.
             var vInput = Input.GetAxis("Vertical");
             var hInput = Input.GetAxis("Horizontal");
-            var walkDirection = (this.cashedTransform.forward * vInput + this.cashedTransform.right * hInput).normalized;
-            var walkVelocity = walkDirection * this.walkSpeed;
+            var walkDirection = (this.m_Transform.forward * vInput + this.m_Transform.right * hInput).normalized;
+            var walkVelocity = walkDirection * this.m_walkSpeed;
 
             // Rigidbodyに反映. (速度制御)
-            this.cashedRigidbody.velocity = gravityVelocity + walkVelocity;
+            this.m_Rigidbody.velocity = gravityVelocity + walkVelocity;
         }
 
         private void OnDrawGizmos()
         {
             Vector3 start = this.transform.position;
-            Vector3 end = start - this.transform.up * this.distanceOnGround;
+            Vector3 end = start - this.transform.up * this.m_distanceOnGround;
             Gizmos.DrawLine(start, end);
         }
     }
---
+++ Assets/_MyAssets/Scripts/Rail/Rail.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rails
+{
+    public class Rail : MonoBehaviour
+    {
+        [SerializeField] private Transform m_goalPlanet;
+        public Transform GoalPlanet => m_goalPlanet;
+    }
+
+}
