551318b3742dcd4d2e298a843562dd3202c8be45
kugimasa <kugimania@gmail.com>
2020-10-19 22:34:22 +0900

    wip

---
+++ Assets/_MyAssets/Scripts/GameManagement/TimeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameManagement
+{
+    public class TimeController : MonoBehaviour
+    {
+        [SerializeField] private float m_initialTime = 30.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+    
+}
